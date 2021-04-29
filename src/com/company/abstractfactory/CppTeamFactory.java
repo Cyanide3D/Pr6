0da0d98ce3d07ed68a180bed60f06474f9ab8a00
Cyanide3D <gamer1997235@yandex.ru>
2021-04-29 20:15:10 +0300

    Initial commit

---
+++ src/com/company/abstractfactory/CppTeamFactory.java
@@ -0,0 +1,19 @@
+package com.company.abstractfactory;
+
+public class CppTeamFactory implements ProjectTeamFactory{
+
+    @Override
+    public Developer getDeveloper() {
+        return new CppDeveloper();
+    }
+
+    @Override
+    public Tester getTester() {
+        return new QUCPPTester();
+    }
+
+    @Override
+    public ProjectManager getProjectManager() {
+        return new CppPM();
+    }
+}
---
+++ src/com/company/factorymethod/Main.java
@@ -0,0 +1,23 @@
+package com.company.factorymethod;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DeveloperFactory developerFactory = getDeveloperFactoryBySpeciality(DeveloperSpeciality.CPP);
+        Developer developer = developerFactory.createDeveloper();
+
+        developer.writeCode();
+    }
+
+    private static DeveloperFactory getDeveloperFactoryBySpeciality(DeveloperSpeciality speciality) {
+        switch (speciality) {
+            case JAVA:
+                return new JavaDeveloperFactory();
+            case CPP:
+                return new CppDeveloperFactory();
+            default:
+                throw new UnsupportedOperationException("Unsupported developer speciality");
+        }
+    }
+
+}
